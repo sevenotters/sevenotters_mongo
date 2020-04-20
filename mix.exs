972c3dc4414812970708002ac9d3008172038097
“nicolafiorillo” <nicola.fiorillo@gmail.com>
2020-04-20 07:25:28 +0200

    Publish first version.

--- mix.exs
+++ mix.exs
@@ -7,7 +7,17 @@ defmodule SevenottersMongo.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      name: "Seven Otters MongoDB support",
+      source_url: "https://github.com/sevenotters",
+      homepage_url: "https://www.sevenotters.org",
+      docs: docs(),
+
+      # Package
+      description: "MongoDB persistence support for Seven Otters.",
+      package: package()
     ]
   end
 
@@ -18,13 +28,30 @@ defmodule SevenottersMongo.MixProject do
     ]
   end
 
+  defp docs do
+    [
+      main: "getting_started",
+      logo: "markdown/icon.png",
+      extras: ["markdown/getting_started.md"]
+    ]
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Nicola Fiorillo"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/sevenotters/sevenotters_mongo"}
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:sevenotters_persistence, path: "../sevenotters_persistence"},
-      {:mongodb, "~> 0.5"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.1", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
+      {:mongodb, "~> 0.5"},
+      {:sevenotters_persistence, "~> 0.1"}
     ]
   end
 end
