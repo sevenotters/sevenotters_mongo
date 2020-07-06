acec09830eca8f3d93b65b2ffa72316e3246f602
Nicola Fiorillo <nicola.fiorillo@gmail.com>
2020-07-06 16:03:32 +0200

    Bump to 0.1.2.

--- lib/mongo.ex
+++ lib/mongo.ex
@@ -11,10 +11,10 @@ defmodule SevenottersMongo.Storage do
     Mongo.start_link(opts ++ [name: __MODULE__, pool_size: @pool_size])
   end
 
-  @spec initialize(String.t()) :: any
+  @spec initialize(bitstring) :: any
   def initialize(_collection), do: nil
 
-  @spec insert(String.t(), Map.t()) :: any
+  @spec insert(bitstring, map) :: any
   def insert(collection, value) do
     {:ok, _id} = Mongo.insert_one(__MODULE__, collection, value)
   end
@@ -22,22 +22,26 @@ defmodule SevenottersMongo.Storage do
   @spec new_id :: any
   def new_id, do: Mongo.object_id()
 
-  @spec printable_id(any) :: String.t()
+  @callback new_printable_id :: bitstring
+  def new_printable_id, do: Mongo.object_id() |> BSON.ObjectId.encode!()
+
+  @spec printable_id(any) :: bitstring
   def printable_id(%BSON.ObjectId{} = id), do: BSON.ObjectId.encode!(id)
   def printable_id(id) when is_bitstring(id), do: id
 
-  @spec object_id(String.t()) :: any
+  @spec object_id(bitstring) :: any
   def object_id(id) do
     {_, bin} = Base.decode16(id, case: :mixed)
     %BSON.ObjectId{value: bin}
   end
 
-  @spec is_valid_id?(any) :: Boolean.t()
+  @spec is_valid_id?(any) :: boolean
   def is_valid_id?(%BSON.ObjectId{} = id),
     do: Regex.match?(@bson_value_format, BSON.ObjectId.encode!(id))
 
-  @spec max_in_collection(String.t(), atom) :: Int.t()
+  @spec max_in_collection(bitstring, atom) :: integer
   def max_in_collection(collection, field) do
+    IO.inspect collection
     Mongo.find(
       __MODULE__,
       collection,
@@ -49,31 +53,31 @@ defmodule SevenottersMongo.Storage do
     |> calculate_max(Atom.to_string(field))
   end
 
-  @spec content(String.t()) :: List.t()
+  @spec content(bitstring) :: [map]
   def content(collection) do
     Mongo.find(__MODULE__, collection, %{}, sort: %{})
     |> Enum.to_list()
   end
 
-  @spec content_of(String.t(), Map.t(), Map.t()) :: List.t()
+  @spec content_of(bitstring, map, map) :: [map]
   def content_of(collection, filter, sort) do
     Mongo.find(__MODULE__, collection, filter, sort: sort)
     |> Enum.to_list()
   end
 
-  @spec content_by_correlation_id(String.t(), String.t(), Map.t()) :: List.t()
+  @spec content_by_correlation_id(bitstring, bitstring, map) :: [map]
   def content_by_correlation_id(collection, correlation_id, sort) do
     Mongo.find(__MODULE__, collection, %{correlation_id: correlation_id}, sort: sort)
     |> Enum.to_list()
   end
 
-  @spec content_by_types(String.t(), [String.t()], Map.t()) :: List.t()
+  @spec content_by_types(bitstring, [bitstring], map) :: [map]
   def content_by_types(collection, types, sort) do
     Mongo.find(__MODULE__, collection, %{type: %{"$in" => types}}, sort: sort)
     |> Enum.to_list()
   end
 
-  @spec drop_collections(List.t()) :: any
+  @spec drop_collections([bitstring]) :: any
   def drop_collections(collections) do
     collections
     |> Enum.each(fn c ->
@@ -84,7 +88,7 @@ defmodule SevenottersMongo.Storage do
   @spec sort_expression() :: any
   def sort_expression(), do: %{counter: 1}
 
-  @spec calculate_max(List.t(), String.t()) :: Int.t()
+  @spec calculate_max([map], bitstring) :: integer
   defp calculate_max([], _field), do: 0
   defp calculate_max([e], field), do: e[field]
 end
