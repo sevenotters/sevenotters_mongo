050fd0498c7400b5352d8b433f0af686c7a2b8b1
Nicola Fiorillo <nicola.fiorillo@gmail.com>
2020-07-26 22:21:48 +0200

    Snapshots.

--- lib/mongo.ex
+++ lib/mongo.ex
@@ -7,16 +7,25 @@ defmodule SevenottersMongo.Storage do
   @bson_value_format ~r/^[A-Fa-f0-9\-]{24}$/
   @pool_size 10
 
+  @events "events"
+  @snapshots "snapshots"
+
   def start_link(opts \\ []) do
     Mongo.start_link(opts ++ [name: __MODULE__, pool_size: @pool_size])
   end
 
-  @spec initialize(bitstring) :: any
-  def initialize(_collection), do: nil
+  @spec initialize() :: any
+  def initialize(), do: nil
 
-  @spec insert(bitstring, map) :: any
-  def insert(collection, value) do
-    {:ok, _id} = Mongo.insert_one(__MODULE__, collection, value)
+  @spec insert_event(map) :: any
+  def insert_event(value) do
+    {:ok, _id} = Mongo.insert_one(__MODULE__, @events, value)
+  end
+
+  @spec upsert_snapshot(map, map) :: any
+  def upsert_snapshot(correlation_id, value) do
+    filter = %{correlation_id: correlation_id}
+    {:ok, _id} = Mongo.update_one(__MODULE__, @snapshots, filter, %{"$set": value}, upsert: true)
   end
 
   @spec new_id :: any
@@ -39,54 +48,56 @@ defmodule SevenottersMongo.Storage do
   def is_valid_id?(%BSON.ObjectId{} = id),
     do: Regex.match?(@bson_value_format, BSON.ObjectId.encode!(id))
 
-  @spec max_in_collection(bitstring, atom) :: integer
-  def max_in_collection(collection, field) do
+  @spec max_counter_in_events() :: integer
+  def max_counter_in_events() do
     Mongo.find(
       __MODULE__,
-      collection,
+      @events,
       %{},
-      sort: %{field => -1},
+      sort: %{:counter => -1},
       limit: 1
     )
     |> Enum.to_list()
-    |> calculate_max(Atom.to_string(field))
+    |> calculate_max(Atom.to_string(:counter))
   end
 
-  @spec content(bitstring) :: [map]
-  def content(collection) do
-    Mongo.find(__MODULE__, collection, %{}, sort: %{})
+  @spec events() :: [map]
+  def events() do
+    Mongo.find(__MODULE__, @events, %{}, sort: %{}) # TODO: streaming with cursor?
     |> Enum.to_list()
   end
 
-  @spec content_of(bitstring, map, map) :: [map]
-  def content_of(collection, filter, sort) do
-    Mongo.find(__MODULE__, collection, filter, sort: sort)
+  @spec snapshots() :: [map]
+  def snapshots() do
+    Mongo.find(__MODULE__, @snapshots, %{}, sort: %{}) # TODO: streaming with cursor?
     |> Enum.to_list()
   end
 
-  @spec content_by_correlation_id(bitstring, bitstring, map) :: [map]
-  def content_by_correlation_id(collection, correlation_id, sort) do
-    Mongo.find(__MODULE__, collection, %{correlation_id: correlation_id}, sort: sort)
+  @spec events_by_correlation_id(bitstring) :: [map]
+  def events_by_correlation_id(correlation_id) do
+    Mongo.find(__MODULE__, @events, %{correlation_id: correlation_id}, sort: %{counter: 1})
     |> Enum.to_list()
   end
 
-  @spec content_by_types(bitstring, [bitstring], map) :: [map]
-  def content_by_types(collection, types, sort) do
-    Mongo.find(__MODULE__, collection, %{type: %{"$in" => types}}, sort: sort)
+  @spec events_by_types([bitstring]) :: [map]
+  def events_by_types(types) do
+    Mongo.find(__MODULE__, @events, %{type: %{"$in" => types}}, sort: %{counter: 1})
     |> Enum.to_list()
   end
 
-  @spec drop_collections([bitstring]) :: any
-  def drop_collections(collections) do
-    collections
-    |> Enum.each(fn c ->
-      Mongo.command(__MODULE__, %{:drop => c}, pool: DBConnection.Poolboy)
-    end)
+  @spec drop_events() :: any
+  def drop_events() do
+    Mongo.command(__MODULE__, %{:drop => @events}, pool: DBConnection.Poolboy)
   end
 
-  @spec sort_expression() :: any
-  def sort_expression(), do: %{counter: 1}
+  @spec drop_snapshots() :: any
+  def drop_snapshots() do
+    Mongo.command(__MODULE__, %{:drop => @snapshots}, pool: DBConnection.Poolboy)
+  end
 
+  #
+  # Privates
+  #
   @spec calculate_max([map], bitstring) :: integer
   defp calculate_max([], _field), do: 0
   defp calculate_max([e], field), do: e[field]
